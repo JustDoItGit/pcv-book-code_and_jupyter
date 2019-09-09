c8703c424bca6add1ccfdc85d3ea8b55b8f81391
cool <cool_kj@126.com>
2019-09-09 16:07:59 +0800

    图像导数

--- MacOs/ch01/ch01_fig1-11_scipy_gauss_deriv.py
+++ MacOs/ch01/ch01_fig1-11_scipy_gauss_deriv.py
@@ -1,12 +1,14 @@
- # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 from PIL import Image
 from pylab import *
 from scipy.ndimage import filters
 import numpy
 
 # 添加中文字体支持
-#from matplotlib.font_manager import FontProperties
-#font = FontProperties(fname=r"c:\windows\fonts\SimSun.ttc", size=14)
+from matplotlib.font_manager import FontProperties
+
+font = FontProperties(fname='../../fonts/SimHei.ttc', size=14)
+
 
 def imx(im, sigma):
     imgx = zeros(im.shape)
@@ -22,31 +24,31 @@ def imy(im, sigma):
 
 def mag(im, sigma):
     # there's also gaussian_gradient_magnitude()
-    #mag = numpy.sqrt(imgx**2 + imgy**2)
+    # mag = numpy.sqrt(imgx**2 + imgy**2)
     imgmag = 255 - numpy.sqrt(imgx ** 2 + imgy ** 2)
     return imgmag
 
 
-im = array(Image.open('../data/empire.jpg').convert('L'))
+im = array(Image.open('../../data/empire.jpg').convert('L'))
 figure()
 gray()
 
 sigma = [2, 5, 10]
 
-for i in  sigma:
-    subplot(3, 4, 4*(sigma.index(i))+1)
+for i in sigma:
+    subplot(3, 4, 4 * (sigma.index(i)) + 1)
     axis('off')
     imshow(im)
-    imgx=imx(im, i)
-    subplot(3, 4, 4*(sigma.index(i))+2)
+    imgx = imx(im, i)
+    subplot(3, 4, 4 * (sigma.index(i)) + 2)
     axis('off')
     imshow(imgx)
-    imgy=imy(im, i)
-    subplot(3, 4, 4*(sigma.index(i))+3)
+    imgy = imy(im, i)
+    subplot(3, 4, 4 * (sigma.index(i)) + 3)
     axis('off')
     imshow(imgy)
-    imgmag=mag(im, i)
-    subplot(3, 4, 4*(sigma.index(i))+4)
+    imgmag = mag(im, i)
+    subplot(3, 4, 4 * (sigma.index(i)) + 4)
     axis('off')
     imshow(imgmag)
 
